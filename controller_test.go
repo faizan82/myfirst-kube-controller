a44b22f3470e1d0eb1948fbd75d1843ca74cbfb4
faizan.ali <mohammad.faizanAli@pearson.com>
2020-06-04 15:02:32 -0700

    completed controller

---
+++ controller_test.go
@@ -0,0 +1,7 @@
+package main
+
+// import "testing"
+
+// func TestNewController(t *testing.T) {
+// 	mycontroller := NewController(cnatClientset versioned.Interface, kubeClientset kubernetes.Interface, atInformer v1alpha1.AtInformer, podInformer v1.PodInformer)
+// }
